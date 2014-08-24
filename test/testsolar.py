2d5914626e23a2a404fad9ada4a69944a38d005f
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2014-08-24 06:49:38 +0000

    change module name to lowercase

--- test/testsolar.py
+++ test/testsolar.py
@@ -19,7 +19,7 @@
 #    You should have received a copy of the GNU General Public License along
 #    with Pysolar. If not, see <http://www.gnu.org/licenses/>.
 
-from Pysolar import \
+from pysolar import \
 	solar, \
 	constants, \
 	julian, \
