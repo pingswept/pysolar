430ea9b934f26af666c3177cb2cf7340be21c89c
Jade Deane <jade.deane@gmail.com>
2017-01-07 17:24:53 -0800

    Add leap second for 2016 and adjust formatting of 2015’s entry.

--- pysolar/time.py
+++ pysolar/time.py
@@ -80,7 +80,8 @@ leap_seconds_adjustments = \
       (+1, 0), # 2012
       (0, 0), # 2013
       (0, 0), # 2014
-      (+1, 0), #2015
+      (+1, 0), # 2015
+      (+1, 0), # 2016
     ]
 
 def get_leap_seconds(when) :
