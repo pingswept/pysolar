acbf51ba3954e23cd51a375e7455672772be67ad
Douglas G. Allen <kb9agt@gmail.com>
2017-01-16 05:20:59 -0600

    Update rise_set_times.py

--- usage_example/rise_set_times.py
+++ usage_example/rise_set_times.py
@@ -1,13 +1,16 @@
+
+
 import datetime
 import pytz
 from pysolar import util
 
 LAT = 51.477811 # approximate Greenwich Maritime Museum
 LON = -0.001475 # new prime merridian :D
-local_system_utc = datetime.datetime.utcnow()
-# print(local_system_utc)
-local_system_utc = pytz.utc.localize(local_system_utc)
-print('system time now UTC:', local_system_utc)
-sun_rise, sun_set = util.get_sunrise_sunset(LAT, LON, local_system_utc)
-print('sunrise: ', sun_rise)
-print('sunset:', sun_set)
+
+UTC = datetime.datetime.utcnow()
+UTC = pytz.utc.localize(UTC)
+SUNRISE, SUNSET = util.get_sunrise_sunset(LAT, LON, UTC)
+
+print('system time now UTC:', UTC)
+print('sunrise: ', SUNRISE)
+print('sunset:', SUNSET)
