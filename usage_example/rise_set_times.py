845e508b9e13e00624d0a334d04cda66635fba51
Douglas G. Allen <kb9agt@gmail.com>
2017-01-16 10:28:54 -0600

    Update rise_set_times.py

--- usage_example/rise_set_times.py
+++ usage_example/rise_set_times.py
@@ -12,7 +12,6 @@ UTC = DT.datetime.utcnow()
 UTC = pytz.utc.localize(UTC)
 SUNRISE, SUNSET = util.get_sunrise_sunset(LAT, LON, UTC)
 
-# print('system time now UTC:', UTC)
 print(DT.date(UTC.year, UTC.month, UTC.day))
 print('sunrise: ', SUNRISE.strftime('%H:%M:%S'))
 print('sunset:  ', SUNSET.strftime('%H:%M:%S'))
