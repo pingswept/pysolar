ae071b793e3f6cb76bc2655e3c50ec680f933c7d
Douglas <kb9agt@gmail.com>
2017-01-21 21:06:35 -0600

    renamed jce, jde, jme to jec, jed, jem

--- test/testsolar.py
+++ test/testsolar.py
@@ -26,8 +26,8 @@ from pysolar import \
     constants, \
     time, \
     elevation
- # R0902: Too many instance attributes 7 is recommended
- # R0904: Too many public methods 20 is recommended
+# R0902: Too many instance attributes 7 is recommended
+# R0904: Too many public methods 20 is recommended
 class TestSolar(unittest.TestCase):
     """ Test solar and time methods """
     def setUp(self):
@@ -40,9 +40,9 @@ class TestSolar(unittest.TestCase):
             self.dio) - time.tt_offset - time.get_leap_seconds(self.dio))
         self.dio += datetime.timedelta(seconds=time.get_delta_t(
             self.dio) - time.tt_offset - time.get_leap_seconds(self.dio))
-          # Reda & Andreas say that this time is in "Local Standard Time", which they
-          # define as 7 hours behind UT (not UTC). Hence the adjustment to convert UT
-          # to UTC.
+        # Reda & Andreas say that this time is in "Local Standard Time", which they
+        # define as 7 hours behind UT (not UTC). Hence the adjustment to convert UT
+        # to UTC.
 
         self.longitude = -105.1786
         self.lon_offset = self.longitude / 360.0
@@ -56,12 +56,12 @@ class TestSolar(unittest.TestCase):
         self.ajd = time.ajd(self.dio) - self.lon_offset
         self.jsd = time.get_julian_solar_day(self.dio)
         self.jct = time.get_julian_century(self.jsd)
-        self.jde = time.get_julian_ephemeris_day(self.dio)
-        self.jce = time.get_julian_ephemeris_century(self.jde)
-        self.jem = time.get_julian_ephemeris_millennium(self.jce)
+        self.jed = time.get_julian_ephemeris_day(self.dio)
+        self.jec = time.get_julian_ephemeris_century(self.jed)
+        self.jem = time.get_julian_ephemeris_millennium(self.jec)
         self.geo_lon = solar.get_geocentric_longitude(self.jem)
         self.geo_lat = solar.get_geocentric_latitude(self.jem)
-        self.nut = solar.get_nutation(self.jce)
+        self.nut = solar.get_nutation(self.jct)
         self.ast = solar.get_apparent_sidereal_time(self.jsd, self.jem, self.nut)
         self.sed = solar.get_sun_earth_distance(self.jem)
         self.teo = solar.get_true_ecliptic_obliquity(self.jem, self.nut)
@@ -103,8 +103,8 @@ class TestSolar(unittest.TestCase):
         self.assertAlmostEqual(318.5119, self.ast, 2)
 
     def test_get_asl(self):
-        """ 204.0085537528 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is 204.297641 at 19:30 and 204.008183 at 12:30 """
+        # value from Reda and Andreas (2005)
         # self.assertAlmostEqual(204.0085537528, self.apparent_sun_longitude, 10)
         self.assertAlmostEqual(204.0085537528, self.asl, 4)
 
@@ -118,98 +118,105 @@ class TestSolar(unittest.TestCase):
         """
         self.assertEqual(datetime.timedelta(0, 0, 357500), self.dut1)
 
+    def test_get_glat(self):
+        """ MIDC SPA is 0.000106 at 19:30 and  at 12:30 0.000101"""
+        # value from Reda and Andreas (2005)
+        # self.assertAlmostEqual(0.0001011219, self.geocentric_latitude, 9)
+        # above fails with more accurate Julian Ephemeris correction
+        self.assertAlmostEqual(0.0001011219, self.geo_lat, 8)
+
     def test_get_glon(self):
-        """ 204.0182635175 """
+        """ MIDC SPA is 204.307346 at 19:30 and  at 12:30 204.017893 """
         # value from Reda and Andreas (2005)
         # self.assertAlmostEqual(204.0182635175, self.geocentric_longitude, 10)
         self.assertAlmostEqual(204.0182635175, self.geo_lon, 4)
 
-    def test_get_glat(self):
-        """ 0.0001011219 """
-         # value from Reda and Andreas (2005)
-         # self.assertAlmostEqual(0.0001011219, self.geocentric_latitude, 9)
-         # above fails with more accurate Julian Ephemeris correction
-        self.assertAlmostEqual(0.0001011219, self.geo_lat, 8)
-
     def test_get_gsd(self):
-        """ -9.31434 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is -9.420685 at 19:30 and  at 12:30 -9.314204 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(-9.31434, self.gsd, 4)
 
     def test_get_gsra(self):
-        """ 202.22741 """
+        """ MIDC SPA is 202.499859 at 19:30 and at 12:30 202.227060 """
         # value from Reda and Andreas (2005)
         self.assertAlmostEqual(202.22741, self.gsra, 4)
 
     def test_get_aoi(self):
-        """ 25.18700 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is 121.988266 at 19:30 and at 12:30 25.108613 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(25.18700, self.aoi, 3)
 
     def test_get_jct(self):
-        """ 0.03792779869191517 """
+        """ MIDC SPA is 0.037936 at 19:30 and 0.037928 at 12:30 """
         self.assertAlmostEqual(0.03792779869191517, self.jct, 12) # value not validated
 
     def test_get_jdn(self):
         """ 2452930.292162778 has longitude added """
         self.assertAlmostEqual(2452930.292162778, self.jdn, 12)
 
-    def test_get_jed(self):
-        """ 2452930.3136 """
-        self.assertAlmostEqual(2452930.3136, self.jde, 4) # value not validated
+    def test_get_jec(self):
+        """ MIDC SPA is 0.037936 at 19:30 and 0.037928 at 12:30 """
+        self.assertAlmostEqual(0.0379278191438864, self.jec, 12) # value not validated
 
-    def test_jem(self):
-        """ 0.0037927819143886397 """
+    def test_get_jed(self):
+        """ MIDC SPA is 2452930.604918 at 19:30 and 2452930.313251 at 12:30 """
+        self.assertAlmostEqual(2452930.3135942305, self.jed, 6) # value not validated
+
+    def test_get_jem(self):
+        """ MIDC SPA is 0.003794 at 19:30 and 0.003793 at 12:30 """
         self.assertAlmostEqual(0.0037927819143886397, self.jem, 12) # value not validated
 
     def test_get_jsd(self):
-        """ 2452930.312847 """
+        """ MIDC SPA is 2452930.604171 at 19:30 and 2452930.312504 at 12:30 """
         # value from Reda and Andreas (2005)
         self.assertAlmostEqual(2452930.312847222, self.jsd, 12)
 
     def test_get_lha(self):
-        """ 11.105900 """
+        """ MIDC SPA is 115.997087 at 19:30 and 10.982401 at 12:30 """
         # value from Reda and Andreas (2005)
         self.assertAlmostEqual(11.105900, self.lha, 4)
 
     def test_get_nut(self):
-        """ -0.00399840 """
-         # value from Reda and Andreas (2005)
-        self.assertAlmostEqual(0.00166657, self.nut['obliquity'], 8)
-         # value from Reda and Andreas (2005)
-        self.assertAlmostEqual(-0.00399840, self.nut['longitude'], 8)
+        """
+        MIDC SPA is 0.001668 at 19:30 and 0.001667 at 12:30
+        MIDC SPA is-0.003993  at 19:30 and -0.003998 at 12:30
+        """
+        # value from Reda and Andreas (2005)
+        self.assertAlmostEqual(0.00166657, self.nut['obliquity'], 12)
+        # value from Reda and Andreas (2005)
+        self.assertAlmostEqual(-0.003998417958822872, self.nut['longitude'], 12)
 
     def test_get_psra(self):
-        """ -0.0003659911495454668 """
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
         self.assertAlmostEqual(-0.0003659911495454668, self.psra, 12)
 
     def test_get_prd(self):
-        """ 0.7702006 """
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
         self.assertAlmostEqual(0.7702006, self.prd, 6) # value not validated
 
     def test_get_pwe(self):
-        """ 83855.90228 """
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
         self.assertAlmostEqual(83855.90228, self.pwe, 4)
 
     def test_get_sed(self):
-        """ 0.9965421031 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(0.9965421031, self.sed, 7)
 
     def test_get_taa(self):
-        """ 194.34024 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         # self.assertAlmostEqual(194.34024, self.topocentric_azimuth_angle, 5)
         self.assertAlmostEqual(194.34024, self.taa, 4)
 
     def test_get_teo(self):
-        """ 23.440465 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(23.440465, self.teo, 6)
 
     def test_get_tlha(self):
-        """ 11.10629 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(11.10629, self.tlha, 4)
 
     def test_get_ts(self):
@@ -221,22 +228,22 @@ class TestSolar(unittest.TestCase):
         print('no tzinfo', time.timestamp(no_tzinfo))
 
     def test_get_tsd(self):
-        """ -9.316179 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(-9.316179, self.tsd, 3)
 
     def test_get_tsra(self):
-        """ 202.22741 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(202.22741, self.tsra, 3)
 
     def test_get_twe(self):
-        """ 277.9600 """
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
         self.assertAlmostEqual(277.9600, self.twe, 4)
 
     def test_get_tza(self):
-        """ MID SPA has 115.923120 """
-         # value from Reda and Andreas (2005)
+        """ MIDC SPA is  at 19:30 and  at 12:30 """
+        # value from Reda and Andreas (2005)
         self.assertAlmostEqual(50.11162, self.tza, 3)
 
 if __name__ == "__main__":
