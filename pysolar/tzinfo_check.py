9bbbf79c17d103e3ad9df436e86364e9dc06e701
JacquotLeHaricot <louis.redonnet@gmail.com>
2018-03-27 00:02:44 +0200

    added tests when an error is NOT expectedwq

--- pysolar/tzinfo_check.py
+++ pysolar/tzinfo_check.py
@@ -18,9 +18,60 @@ You have to specify the 'tzinfo' attribute of \
 
 
 def check_aware_dt(*argnames):
+  """Returns a decorator that makes sure that 
+  all the arguments in 'argnames', are 'datetime.datetime' objects
+  with a non-null tzinfo attribute
+
+  Parameters
+  ----------
+  argnames : List[str]
+    list of names from the arguments of function 'func' 
+    (the decorated function) that are supposed to only accept
+    as values 'datetime.datetime' objects 
+    with a non-null tzinfo attribute
+
+  Returns
+  -------
+  checker : function
+    decorator that makes sure that every argument of the decorated
+    function that is in 'argnames' is given only 'datetime.datetime' objects
+    with non-null tzinfo attribute as values"""
   def checker(func):
-    @wraps(func)
+    """Decorator (see above in the `Returns` section)
+
+    Parameters
+    ----------
+    func : function
+      decorated function
+      every argument of 'func' that is in argnames
+      will only accept tz-aware 'datetime' objects
+
+    Returns
+    -------
+    func_with_check : function
+      decorated function, basically the same function as 'func', 
+      with the check for tz-awareness performed before execution"""
+    @wraps(func, updated=WRAPPER_UPDATES + ('__doc__',))
     def func_with_check(*args, **kwargs):
+      """Decorated function ; will be, apart from the check, 
+      completely identical to the 'func' function
+      We search for 'argnames' values in args and kwargs
+
+      Parameters
+      ----------
+      args : list
+        list of positional arguments that are supposedly 
+        passed to the 'func' function
+      kwargs : dict
+        dict of keyword arguments that are supposedly passed
+        to the 'func' function
+
+      Returns
+      -------
+      func(*args, **kwargs)
+        we return the very same result that would have been returned
+        by the 'func' function alone
+        we just checked the values of args from argnames before"""
       for argname in argnames:
         # first checking if argname is a valid arg name
         full = inspect.getfullargspec(func)
--- test/test_datetime_tzinfo.py
+++ test/test_datetime_tzinfo.py
@@ -29,110 +29,293 @@ import unittest
 from pysolar.tzinfo_check import NoTimeZoneInfoError
 
 
-class TestTimeZone(unittest.TestCase):
+class TestErrorTimeZoneIsNone(unittest.TestCase):
   unaware = datetime.datetime(2000, 1, 1)
-  aware = unaware.replace(tzinfo=pytz.utc)
 
   lat = 0.0
   lon = 0.0
 
-  def test_solar_topocentric_position(self):
+  def test_solar_topocentric_position_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_topocentric_position(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_position(self):
+  def test_solar_get_position_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_position(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_altitude(self):
+  def test_solar_get_altitude_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_altitude(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_altitude_fast(self):
+  def test_solar_get_altitude_fast_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_altitude_fast(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_azimuth(self):
+  def test_solar_get_azimuth_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_azimuth(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_azimuth_fast(self):
+  def test_solar_get_azimuth_fast_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_azimuth_fast(self.lat, self.lon, self.unaware)
 
-  def test_solar_get_hour_angle(self):
+  def test_solar_get_hour_angle_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_hour_angle(self.unaware, self.lon)
 
-  def test_solar_get_solar_time(self):
+  def test_solar_get_solar_time_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solar.get_hour_angle(self.unaware, self.lon)
 
-  def test_solartime_get_leap_seconds(self):
+  def test_solartime_get_leap_seconds_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solartime.get_leap_seconds(self.unaware)
 
-  def test_solartime_get_delta_t(self):
+  def test_solartime_get_delta_t_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solartime.get_delta_t(self.unaware)
 
-  def test_solartime_get_julian_solar_day(self):
+  def test_solartime_get_julian_solar_day_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solartime.get_julian_solar_day(self.unaware)
 
-  def test_solartime_get_julian_ephemeris_day(self):
+  def test_solartime_get_julian_ephemeris_day_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       solartime.get_julian_ephemeris_day(self.unaware)
 
-  def test_util_get_sunrise_sunset(self):
+  def test_util_get_sunrise_sunset_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.get_sunrise_sunset(self.lat, self.lon, self.unaware)
 
-  def test_util_get_sunrise_time(self):
+  def test_util_get_sunrise_time_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.get_sunrise_time(self.lat, self.lon, self.unaware)
 
-  def test_util_get_sunset_time(self):
+  def test_util_get_sunset_time_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.get_sunset_time(self.lat, self.lon, self.unaware)
 
-  def test_util_mean_earth_sun_distance(self):
+  def test_util_mean_earth_sun_distance_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.mean_earth_sun_distance(self.unaware)
 
-  def test_util_extraterrestrial_irrad(self):
+  def test_util_extraterrestrial_irrad_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.extraterrestrial_irrad(self.lat, self.lon, self.unaware)
 
-  def test_util_declination_degree(self):
+  def test_util_declination_degree_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.declination_degree(self.unaware)
 
-  def test_util_solarelevation_function_overcast(self):
+  def test_util_solarelevation_function_overcast_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.solarelevation_function_overcast(self.lat, self.lon, self.unaware)
 
-  def test_util_diffuse_underclear(self):
+  def test_util_diffuse_underclear_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.diffuse_underclear(self.lat, self.lon, self.unaware)
 
-  def test_util_diffuse_underovercast(self):
+  def test_util_diffuse_underovercast_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.diffuse_underovercast(self.lat, self.lon, self.unaware)
 
-  def test_util_direct_underclear(self):
+  def test_util_direct_underclear_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.direct_underclear(self.lat, self.lon, self.unaware)
 
-  def test_util_global_irradiance_clear(self):
+  def test_util_global_irradiance_clear_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.global_irradiance_clear(self.lat, self.lon, self.unaware)
 
-  def test_util_global_irradiance_overcast(self):
+  def test_util_global_irradiance_overcast_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
       util.global_irradiance_overcast(self.lat, self.lon, self.unaware)
 
-  def test_util_clear_index(self):
+  def test_util_clear_index_raise_error(self):
     with self.assertRaises(NoTimeZoneInfoError):
-      ghi_data = Ellipsis # Don't know what ghi_data is supposed to be
+      ghi_data = [0, 0] # Don't know what ghi_data is supposed to be
       util.clear_index(ghi_data, self.lat, self.lon, self.unaware)
+
+
+
+class TestTimeZoneNotNone(unittest.TestCase):
+  aware = datetime.datetime(2000, 1, 1, tzinfo=pytz.utc)
+
+  lat = 0.0
+  lon = 0.0
+
+  def test_solar_topocentric_position_no_error(self):
+    try:
+      solar.get_topocentric_position(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_position_no_error(self):
+    try:
+      solar.get_position(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_altitude_no_error(self):
+    try:
+      solar.get_altitude(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_altitude_fast_no_error(self):
+    try:
+      solar.get_altitude_fast(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_azimuth_no_error(self):
+    try:
+      solar.get_azimuth(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_azimuth_fast_no_error(self):
+    try:
+      solar.get_azimuth_fast(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_hour_angle_no_error(self):
+    try:
+      solar.get_hour_angle(self.aware, self.lon)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solar_get_solar_time_no_error(self):
+    try:
+      solar.get_hour_angle(self.aware, self.lon)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solartime_get_leap_seconds_no_error(self):
+    try:
+      solartime.get_leap_seconds(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solartime_get_delta_t_no_error(self):
+    try:
+      solartime.get_delta_t(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solartime_get_julian_solar_day_no_error(self):
+    try:
+      solartime.get_julian_solar_day(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_solartime_get_julian_ephemeris_day_no_error(self):
+    try:
+      solartime.get_julian_ephemeris_day(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_get_sunrise_sunset_no_error(self):
+    try:
+      util.get_sunrise_sunset(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_get_sunrise_time_no_error(self):
+    try:
+      util.get_sunrise_time(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_get_sunset_time_no_error(self):
+    try:
+      util.get_sunset_time(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_mean_earth_sun_distance_no_error(self):
+    try:
+      util.mean_earth_sun_distance(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_extraterrestrial_irrad_no_error(self):
+    try:
+      util.extraterrestrial_irrad(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_declination_degree_no_error(self):
+    try:
+      util.declination_degree(self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_solarelevation_function_overcast_no_error(self):
+    try:
+      util.solarelevation_function_overcast(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_diffuse_underclear_no_error(self):
+    try:
+      util.diffuse_underclear(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_diffuse_underovercast_no_error(self):
+    try:
+      util.diffuse_underovercast(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_direct_underclear_no_error(self):
+    try:
+      util.direct_underclear(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_global_irradiance_clear_no_error(self):
+    try:
+      util.global_irradiance_clear(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_global_irradiance_overcast_no_error(self):
+    try:
+      util.global_irradiance_overcast(self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
+
+  def test_util_clear_index(self):
+    try:
+      ghi_data = [0, 0] # Don't know what ghi_data is supposed to be
+      util.clear_index(ghi_data, self.lat, self.lon, self.aware)
+    except NoTimeZoneInfoError:
+      self.fail("""'NoTimeZoneInfoError' should not be raised \
+as 'datetime' object is tz-aware.""")
